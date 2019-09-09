bdc0172e988afee2d63a63d12b0a1646e7448efd
Dion Earle <dion.earle@gmail.com>
2019-09-09 21:39:00 +1000

    View previous commit

--- static/js/library.js
+++ static/js/library.js
@@ -2,10 +2,12 @@
 let url = new URL('http://ws.audioscrobbler.com/2.0/');
 
 // since the API requires url parameters, we set these here
-let query = {method: 'library.getartists',
+let query = {
+    method: 'library.getartists',
     api_key: '3e0c61f86ab0621665f8bb0bccd2eaf9',
     user: 'dionearle',
-    format: 'json'};
+    format: 'json'
+};
 
 // we then add this search query to the url
 url.search = new URLSearchParams(query);
@@ -26,19 +28,76 @@ fetch(url, options)
         const artists = response.artists;
 
         for (let i = 0; i < artists.artist.length; i++) {
-            
-            display_artist(artists.artist[i])
+
+            display_artist(artists.artist[i]);
+
         }
+
+        // HERE WE DISPLAY THE ARTIST'S RELEASES!!!
+        get_releases(artists.artist[0].name);
+
     });
 
 function display_artist(artist) {
 
     const html = document.getElementById('artists');
 
-    // here we create the post element
     const thisArtist = document.createElement('li');
-    thisArtist.classList.add('artist');  
+    thisArtist.classList.add('artist');
     thisArtist.textContent = artist.name + ' has been played ' + artist.playcount + ' times.';
 
     html.appendChild(thisArtist);
+}
+
+function get_releases(artist) {
+
+    // here we specify the url for the fetch request
+    let url = new URL('https://musicbrainz.org/ws/2/release-group/');
+
+    // since the API requires url parameters, we set these here
+    let params = {
+        query: `artist\:\"${artist}\"`
+    };
+
+    // we then add this search query to the url
+    url.search = new URLSearchParams(params);
+
+    // here we simply setup the options for the fetch request
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // we now fetch the data
+    fetch(url, options)
+        .then(response => response.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(response => {
+
+            let releases = response.getElementsByTagName('release-group');
+
+            for(let i = 0; i < releases.length; i++) {
+
+                let release = releases[i];
+                let type = release.attributes.type.textContent;
+
+                let titleObject = release.getElementsByTagName('title');
+                let title = titleObject[0].textContent;
+
+                display_release(title, type);
+            }
+        });
+}
+
+function display_release(title, type) {
+
+    const html = document.getElementById('artists');
+
+    const thisRelease = document.createElement('li');
+    thisRelease.classList.add('release');
+    thisRelease.textContent = title + ' - ' + type;
+
+    html.appendChild(thisRelease);
 }
