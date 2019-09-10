ba3aef8d1d7416b0eca7cc271ded7e8e1ae358f2
Dion Earle <dion.earle@gmail.com>
2019-09-10 18:15:25 +1000

    Attempted to use discogs API to retrieve releases from an artist, and succeeded before realising these releases did not store the exact date of their release. Instead, I went back to using musicbrainz API and managed to get the JSON working, alongside accessing all releases from an artist and the exact release date for each. Using this I also implemented a function which would only display albums made within the past year. Still have the issue of not being able to fetch releases from multiple artists at once as it gives an error.

--- static/js/library.js
+++ static/js/library.js
@@ -30,11 +30,18 @@ fetch(url, options)
         for (let i = 0; i < artists.artist.length; i++) {
 
             display_artist(artists.artist[i]);
-
         }
 
-        // HERE WE DISPLAY THE ARTIST'S RELEASES!!!
-        get_releases(artists.artist[0].name);
+        //PLACEHOLDER! TESTING WITH ONLY FIRST FEW ARTISTS!
+        // (NOTE ISSUE WHEN TRYING TO FETCH FOR MULTIPLE ARTISTS AT ONCE)
+        get_releases(artists.artist[1].name);
+        /*get_releases(artists.artist[1].name);
+        get_releases(artists.artist[2].name);
+        get_releases(artists.artist[3].name);
+        get_releases(artists.artist[4].name);
+        get_releases(artists.artist[5].name);
+        get_releases(artists.artist[6].name);
+        get_releases(artists.artist[7].name); */
 
     });
 
@@ -49,14 +56,16 @@ function display_artist(artist) {
     html.appendChild(thisArtist);
 }
 
+// USING MUSICBRAINZ!!!
 function get_releases(artist) {
 
     // here we specify the url for the fetch request
-    let url = new URL('https://musicbrainz.org/ws/2/release-group/');
+    let url = new URL('https://musicbrainz.org/ws/2/artist/');
 
     // since the API requires url parameters, we set these here
     let params = {
-        query: `artist\:\"${artist}\"`
+        query: `artist\:\"${artist}\"`,
+        fmt: 'json'
     };
 
     // we then add this search query to the url
@@ -72,32 +81,126 @@ function get_releases(artist) {
 
     // we now fetch the data
     fetch(url, options)
-        .then(response => response.text())
-        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(response => response.json())
         .then(response => {
 
-            let releases = response.getElementsByTagName('release-group');
+            // here we extract the artistID from the response
+            let artistID = response.artists[0].id;
 
-            for (let i = 0; i < releases.length; i++) {
+            // here we specify the url for the fetch request
+            let url2 = new URL('https://musicbrainz.org/ws/2/release/');
 
-                let release = releases[i];
-                let type = release.attributes.type.textContent;
+            // since the API requires url parameters, we set these here
+            let params2 = {
+                artist: `${artistID}`,
+                inc: 'release-groups',
+                limit: '100',
+                fmt: 'json'
+            };
 
-                let titleObject = release.getElementsByTagName('title');
-                let title = titleObject[0].textContent;
+            // we then add this search query to the url
+            url2.search = new URLSearchParams(params2);
 
-                display_release(title, type);
+            // here we simply setup the options for the fetch request
+            const options = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             }
+
+            // we now fetch the data
+            fetch(url2, options)
+                .then(response => response.json())
+                .then(response => {
+
+                    let releases = response.releases;
+
+                    for (let i = 0; i < releases.length; i++) {
+
+                        let release = releases[i];
+
+                        let date = release.date;
+                        let title = release.title;
+
+                        let isRecentRelease = checkIsRecentRelease(date);
+
+                        if (isRecentRelease) {
+                            display_release(title, date);
+                        }
+                    }
+                });
         });
 }
 
-function display_release(title, type) {
+function display_release(title, date) {
 
     const html = document.getElementById('artists');
 
     const thisRelease = document.createElement('li');
     thisRelease.classList.add('release');
-    thisRelease.textContent = title + ' - ' + type;
+    thisRelease.textContent = title + ' released on ' + date;
 
     html.appendChild(thisRelease);
 }
+
+// TODO: return true if date is within last month OR week
+// otherwise return false
+function checkIsRecentRelease(date) {
+
+    let dateString = date.split('-');
+
+    let year = dateString[0];
+    let month = dateString[1];
+    let day = dateString[2];
+
+    let currentDate = new Date();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentDay = currentDate.getDate();
+    let currentYear = currentDate.getFullYear();screen
+
+    // placeholder test to see if release is in current year
+    if (currentYear == year) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// USING DISCOGS!!! (CAN PROBABLY DELETE SINCE WORKS IN SIMILAR WAY TO
+// MUSICBRAINZ IN THAT IT DOESN'T STORE EXACT DATES FOR ALL RELEASES)
+/*function get_releases(artist) {
+
+    // here we specify the url for the fetch request
+    let url = new URL('https://api.discogs.com/database/search');
+
+    // since the API requires url parameters, we set these here
+    let params = {
+        artist: artist,
+        year: new Date().getFullYear()
+    };
+
+    // we then add this search query to the url
+    url.search = new URLSearchParams(params);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Discogs token=ssgaMRuuruKbkzUQhdsgLDcaJKQaRvDzSeXxqnhi',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // we now fetch the data
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response => {
+
+            let results = response.results;
+
+            for (let i = 0; i < results.length; i++) {
+
+                display_release(results[i].title);
+            } 
+        });
+}*/
